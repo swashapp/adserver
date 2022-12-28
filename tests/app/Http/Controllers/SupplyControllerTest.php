c7994168b0b99e5ab4a1d89ac30f0c1a2d5e4687
mabdi <newmrd@gmail.com>
2022-12-28 18:55:11 +0000

    patch js

--- tests/app/Http/Controllers/SupplyControllerTest.php
+++ tests/app/Http/Controllers/SupplyControllerTest.php
@@ -63,6 +63,7 @@ final class SupplyControllerTest extends TestCase
     private const PAGE_WHY_URI = '/supply/why';
     private const REPORT_AD_URI = '/supply/ad/report';
     private const SUPPLY_ANON_URI = '/supply/anon';
+    private const MAIN_JS_URI = '/main.js';
     private const TARGETING_REACH_URI = '/supply/targeting-reach';
     private const LEGACY_FOUND_BANNERS_STRUCTURE = [
         'id',
@@ -118,6 +119,15 @@ final class SupplyControllerTest extends TestCase
         $response = $this->get(self::PAGE_WHY_URI);
 
         $response->assertStatus(Response::HTTP_UNPROCESSABLE_ENTITY);
+    }
+
+    public function testMainJs(): void
+    {
+        $response = $this->get(self::MAIN_JS_URI);
+
+        $response->assertStatus(Response::HTTP_OK);
+        $js_content = $response->streamedContent();
+        $this->assertStringContainsString(config('app.foreign_default_site_js'), $js_content);
     }
 
     public function testPageWhyInvalidBannerId(): void
