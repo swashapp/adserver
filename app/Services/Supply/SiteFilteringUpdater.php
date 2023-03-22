1477f0a5d255107acffb064c54d95d905d37bc9d
Hashemnik <hashem@swashapp.io>
2023-03-22 17:55:55 +0430

    Feat: User REVIEWER_USER_ID that is set in .env can reject the banners

--- app/Services/Supply/SiteFilteringUpdater.php
+++ app/Services/Supply/SiteFilteringUpdater.php
@@ -63,13 +63,6 @@ class SiteFilteringUpdater
             $siteExcludes[self::INTERNAL_CLASSIFIER_NAMESPACE][] = $excludeKeyword->keyword();
         }
 
-        if(config('app.reviewer_user_id')){
-            $requireKeywords = $this->getClassificationForAcceptedBannersForAllSites(intval(config('app.reviewer_user_id')));
-            foreach ($requireKeywords as $requireKeyword) {
-                $siteExcludes[self::INTERNAL_CLASSIFIER_NAMESPACE][] = $requireKeyword->keyword();
-            }
-        }
-
         $baseRequires = json_decode(config('app.site_filtering_require') ?? '', true);
         if (is_array($baseRequires)) {
             $siteRequires = array_merge_recursive($siteRequires, $baseRequires);
@@ -78,6 +71,13 @@ class SiteFilteringUpdater
         $baseExcludes = json_decode(config('app.site_filtering_exclude') ?? '', true);
         if (is_array($baseExcludes)) {
             $siteExcludes = array_merge_recursive($siteExcludes, $baseExcludes);
+        }
+
+        if(config('app.reviewer_user_id')){
+            $requireKeywords = $this->getClassificationForAcceptedBannersForAllSites(intval(config('app.reviewer_user_id')));
+            foreach ($requireKeywords as $requireKeyword) {
+                $siteExcludes[self::INTERNAL_CLASSIFIER_NAMESPACE][] = $requireKeyword->keyword();
+            }
         }
 
         $site->site_excludes = array_map([__CLASS__, 'normalize'], $siteExcludes);
